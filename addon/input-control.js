f0feecf763da63cb288c2908ecb3f950fd33af5e
William Maunder <william@edalive.com>
2016-09-20 13:06:19 +1000

    Added autocomplete attribute.

--- addon/input-control.js
+++ addon/input-control.js
@@ -2,11 +2,12 @@ import Em from 'ember';
 import ControlMixin from 'ember-idx-forms/mixins/control';
 
 export default Em.TextField.extend(ControlMixin, {
-  attributeBindings: ['placeholder', 'required', 'autofocus', 'disabled'],
+  attributeBindings: ['placeholder', 'required', 'autofocus', 'disabled', 'autocomplete'],
   placeholder: Em.computed.alias('parentView.placeholder'),
   required: Em.computed.alias('parentView.required'),
   autofocus: Em.computed.alias('parentView.autofocus'),
   disabled: Em.computed.alias('parentView.disabled'),
+  autocomplete: Em.computed.alias('parentView.autocomplete'),
   type: Em.computed.alias('parentView.type'),
   model: Em.computed.alias('parentView.model'),
   propertyName: Em.computed.alias('parentView.propertyName')
